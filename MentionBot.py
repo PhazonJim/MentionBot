3fd9ee159791dc4d3765dbb69c8a96920ae9d7e9
Jimothy <phazonjim@gmail.com>
2019-09-20 01:29:23 -0400

    Updated to use pushshift instead of reddit directly

--- MentionBot.py
+++ MentionBot.py
@@ -1,7 +1,11 @@
 import praw
+from psaw import PushshiftAPI
 import os
 import re
 import yaml
+import json
+import time
+from pprint import pprint
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 #===Globals===#
@@ -10,26 +14,64 @@ reddit = None
 #Config file
 config = None
 webhook = None
+api = None
+postCache = None
 
 def init():
     global config
     global reddit
     global webhook
+    global api
+    global postCache
+
+    #Load configs
     config = yaml.load(open('config.yaml').read())
     client = config['client']
+
+    #Set up APIs
     reddit = praw.Reddit(**client)
+    api = PushshiftAPI()
     webhook = webhook = DiscordWebhook(url=config["webhook"])
+    
+    #Load post cache
+    postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
+    with open(postCacheName, 'r') as fin:
+        cache = fin.read()
+    postCache = json.loads(cache)
+
+def saveCache():
+    postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
+    with open(postCacheName, 'w') as fout:
+        for chunk in json.JSONEncoder().iterencode(postCache):
+            fout.write(chunk)
+
+def postWebhook(mentions):
+    for comment in mentions:
+        title = 'Posted on {} by {}'.format(comment.subreddit, comment.author)
+        description = '[Comment]({})'.format('https://www.reddit.com' + comment.permalink)
+        embed = DiscordEmbed(title=title, description=description, color=242424)
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.remove_embed(0)
+        time.sleep(3)
 
 if __name__ == '__main__':
     #Initiate a bunch of stuff
     init()
     #Scan stream of comments to find mentions of word
-    for comment in reddit.subreddit(config['subreddit']).stream.comments():
-        if re.search(config['regex'], comment.body):
-            # create embed object for webhook
-            title = 'Posted on {} by {}'.format(comment.subreddit, comment.author.name)
-            description = '[Comment]({})'.format('https://www.reddit.com' + comment.permalink)
-            embed = DiscordEmbed(title=title, description=description, color=242424)
-            webhook.add_embed(embed)
-            webhook.execute()
-        
+    while(True):
+        try:
+            gen = api.search_comments(q='"'+ config['searchString'] + '"', limit=50)
+            mentions = []
+            for comment in gen:
+                if comment.subreddit != config['ignore'] and comment.id not in postCache:
+                    if config['searchString'] in comment.body.lower():
+                        print("New comment! " + comment.id)
+                        postCache[comment.id] = comment.permalink
+                        mentions.append(comment)
+            if mentions:
+                postWebhook(mentions)
+                saveCache()
+        except:
+            pass
+        time.sleep(60)
