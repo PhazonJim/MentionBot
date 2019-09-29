a0b65864b8ba74ba56a022a44087d8588e990f41
Jimothy <phazonjim@gmail.com>
2019-09-29 19:02:39 -0400

    Refactor

--- MentionBot.py
+++ MentionBot.py
@@ -8,6 +8,10 @@ import textwrap
 from pprint import pprint
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+#===Constants===#
+CONFIG_FILE = os.path.join(os.path.dirname(__file__),'config.yaml')
+CACHE_FILE =  os.path.join(os.path.dirname(__file__), "cache.json")
+
 #===Globals===#
 #Config file
 config = None
@@ -16,54 +20,47 @@ def loadConfig():
     global config
     #Load configs
     try:
-        config = yaml.load(open(os.path.join(os.path.dirname(__file__),'config.yaml')).read(), Loader=yaml.FullLoader)
+        config = yaml.load(open().read(CONFIG_FILE), Loader=yaml.FullLoader)
     except:
         print("'config.yaml' could not be located. Please ensure 'config.example' has been renamed")
         exit()
 
-def saveCache(postCache):
-    postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
-    with open(postCacheName, 'w') as fout:
-        for chunk in json.JSONEncoder().iterencode(postCache):
-            fout.write(chunk)
-
 def loadCache():
     postCache = {"comments":{}, "submissions":{}}
-    postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
     try:
-        with open(postCacheName, 'r') as fin:
-            cache = fin.read()
-        postCache = json.loads(cache)
+        with open(CACHE_FILE, 'r') as fin:
+            postCache = json.load(fin)
     except Exception as e:
         print (e)
     return postCache
 
-def postWebhook(webhook, mentions, postCache, mentionType):
-    for item in mentions:
-        try:            
-            title = 'Posted on {} by {}'.format(item.subreddit, item.author)
-            embed = None
+def saveCache(postCache):
+    with open(CACHE_FILE, 'w') as fout:
+        for chunk in json.JSONEncoder().iterencode(postCache):
+            fout.write(chunk)
 
-            if mentionType == 'comments':
-                description = '[Comment Permalink]({})'.format('https://www.reddit.com' + item.permalink)
-                embed = DiscordEmbed(title=title, description=description, color=242424)
-                embed.add_embed_field(name='Comment Preview:', value=textwrap.shorten(item.body, width=900, placeholder="...(Too long to preview full content)..."))
-            
-            elif mentionType == 'submissions':
-                description = '[Submission Permalink]({})'.format('https://www.reddit.com' + item.permalink)
-                embed = DiscordEmbed(title=title, description=description, color=242424)
-                embed.add_embed_field(name='Submission Title:', value=item.title)
-                embed.add_embed_field(name='Submission Preview:', value=textwrap.shorten(item.selftext, width=900, placeholder="...(Too long to preview full content)...") if item.selftext else 'Submission is a direct link')
-            embed.set_image(url=config['imageUrl'])
-            webhook.add_embed(embed)
-            webhook.execute()
-            webhook.remove_embed(0)
-            postCache[mentionType][item.id] = item.permalink
-            time.sleep(3)
-
-        except Exception as e:
-            print(e)
-            pass
+def postWebhook(webhook, item, postCache, mentionType):
+    try:            
+        title = 'Posted on {} by {}'.format(item.subreddit, item.author)
+        embed = None
+        if mentionType == 'comments':
+            description = '[Comment Permalink]({})'.format('https://www.reddit.com' + item.permalink)
+            embed = DiscordEmbed(title=title, description=description, color=242424)
+            embed.add_embed_field(name='Comment Preview:', value=textwrap.shorten(item.body, width=900, placeholder="...(Too long to preview full content)..."))
+        
+        elif mentionType == 'submissions':
+            description = '[Submission Permalink]({})'.format('https://www.reddit.com' + item.permalink)
+            embed = DiscordEmbed(title=title, description=description, color=242424)
+            embed.add_embed_field(name='Submission Title:', value=item.title)
+            embed.add_embed_field(name='Submission Preview:', value=textwrap.shorten(item.selftext, width=900, placeholder="...(Too long to preview full content)...") if item.selftext else 'Submission is a direct link')
+        embed.set_image(url=config['imageUrl'])
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.remove_embed(0)
+        postCache[mentionType][item.id] = item.permalink
+    except Exception as e:
+        print(e)
+        pass
     return postCache
 
 def queryPushshift(api, postCache, queryType):
@@ -97,11 +94,12 @@ if __name__ == '__main__':
     postCache = loadCache()
     #Scan stream of comments to find mentions of word
     commentMentions = queryPushshift(api, postCache, 'comments')
-    if commentMentions:
-        postCache = postWebhook(webhook, commentMentions, postCache, 'comments')
-        saveCache(postCache)
+    for comment in commentMentions:
+        postCache = postWebhook(webhook, comment, postCache, 'comments')
+        time.sleep(3)
     #Scan stream of submissions to find mentions of word
     submissionMentions = queryPushshift(api, postCache, 'submissions')
-    if submissionMentions:
-        postCache = postWebhook(webhook, submissionMentions, postCache, 'submissions')
-        saveCache(postCache)
+    for submission in submissionMentions:
+        postCache = postWebhook(webhook, submission, postCache, 'submissions')
+        time.sleep(3)
+    saveCache(postCache)
